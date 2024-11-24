4c7896ae4a7ed71e1f1a37ef1201583bb2e2b2f6
Seth S <ssurendonk@gmail.com>
2024-11-23 18:21:47 -0800

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import "./globals.css";
 
 const geistSans = localFont({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
       <Navbar />
       <main>{children}</main>
+      <Footer />
       </body>
     </html>
   );
