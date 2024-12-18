20bccd2d23820d08bf71b7d380b8e2bef96df457
Seth S <ssurendonk@gmail.com>
2024-12-17 21:42:42 -0800

    Add builds configs to disable linting and type checking

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during the build process
+  },
+    typescript: {
+    // Disable type checking during the build process
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
