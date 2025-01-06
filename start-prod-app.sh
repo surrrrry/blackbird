6f89e76970b96386877a2b113bfd3131bb1d9c2c
Seth S <ssurendonk@gmail.com>
2025-01-06 14:22:11 -0800

    Fix start up script

--- start-prod-app.sh
+++ start-prod-app.sh
@@ -3,6 +3,7 @@
 IMAGE_NAME=blackbird
 docker rm -f $IMAGE_NAME || true
 docker rmi -f $IMAGE_NAME ||  true
+cd ~/app
 sh build-image.sh
 
 # https://serverfault.com/questions/238715/routing-port-25-to-another-machine
@@ -10,4 +11,5 @@ docker run -dit -v /dev/shm:/dev/shm --name $IMAGE_NAME --net=host -p 4545:3000 
 rm secrets.json
 rm build-image.sh
 rm Dockerfile
+cd ..
 rm -r app
